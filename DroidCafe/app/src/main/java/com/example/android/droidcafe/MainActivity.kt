a7c0f00b28d305d27fe5910ee12601df4cca9e54
BIratkirat <Gooner10@Birats-MacBook-Pro.local>
2017-12-19 16:52:35 -0500

    converting to kotlin

--- DroidCafe/app/src/main/java/com/example/android/droidcafe/MainActivity.kt
+++ DroidCafe/app/src/main/java/com/example/android/droidcafe/MainActivity.kt
@@ -19,9 +19,7 @@ package com.example.android.droidcafe
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
@@ -47,10 +45,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
 
-//        val fab = findViewById<View>(R.id.fab) as FloatingActionButton
         fab.setOnClickListener { displayMap() }
     }
 
